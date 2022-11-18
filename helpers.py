d930169358d448615cf64e29235ab508946f99e3
DiogoMartinhoISEP <43065113+DiogoMartinhoISEP@users.noreply.github.com>
2022-11-18 08:19:20 +0000

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,18 +1,3 @@
-import numpy as np
-import sys
-import ast
-
 def bar(v):
     return v
     
-def most_frequent(List):
-  counter = 0
-  num = List[0]
-
-  for i in List:
-     curr_frequency = List.count(i)
-     if (curr_frequency > counter):
-        counter = curr_frequency
-        num = i
-
-   return num
