98c4f84e0b309dd2c5e1d35f1786da8576f0a344
DiogoMartinhoISEP <43065113+DiogoMartinhoISEP@users.noreply.github.com>
2022-11-18 07:49:07 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -171,7 +171,7 @@ def getMessage():
 @app.route('/')
 def index():
     # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api! NE244442W</h1>"
+    return "<h1>Welcome to our medium-greeting-api! NE22W</h1>"
 
 
 if __name__ == '__main__':
@@ -179,3 +179,48 @@ if __name__ == '__main__':
     app.run(threaded=True, port=5000)
     
     
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+
+    for i in List:
+        curr_frequency = List.count(i)
+        if (curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+
+    return num
+
+def available_actions(state):
+    current_state_row = R[state,]
+    av_act = np.where(current_state_row >= 0)[1]
+    return av_act
+
+
+available_act = available_actions(initial_state)
+
+
+def sample_next_action(available_actions_range):
+    next_action = int(np.random.choice(available_act, 1))
+    return next_action
+
+
+action = sample_next_action(available_act)
+
+
+def update(current_state, action, gamma):
+    max_index = np.where(Q[action,] == np.max(Q[action,]))[1]
+
+    if max_index.shape[0] > 1:
+        max_index = int(np.random.choice(max_index, size=1))
+    else:
+        max_index = int(max_index)
+    max_value = Q[action, max_index]
+
+    Q[current_state, action] = R[current_state, action] + gamma * max_value
+ #   print('max_value', R[current_state, action] + gamma * max_value)
+
+    if (np.max(Q) > 0):
+        return (np.sum(Q / np.max(Q) * 100))
+    else:
+        return (0)
